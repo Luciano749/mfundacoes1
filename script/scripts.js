6fb1e58929334432affb663511de03f082e417c0
Luciano Roberto <lucianoroberto671@gmail.com>
2022-07-21 15:03:36 -0300

    1

--- script/scripts.js
+++ script/scripts.js
@@ -1,9 +1,30 @@
-var map = L.map("map").setView([51.505, -0.09], 13);
+AOS.init();
+
+// --
+
+let modal = document.getElementsByClassName("modal")[0];
+let responsiveNavButton = document.getElementsByClassName(
+  "responsiveNavButton"
+)[0];
+
+responsiveNavButton.onclick = () => {
+  modal.classList.add("active");
+};
+
+modal.onclick = (e) => {
+  if (e.currentTarget === e.target) {
+    modal.classList.remove("active");
+  }
+};
+
+// ---
+
+let map = L.map("map").setView([51.505, -0.09], 13);
+let marker = L.marker([51.5, -0.09]).addTo(map);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution: "© OpenStreetMap",
 }).addTo(map);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
